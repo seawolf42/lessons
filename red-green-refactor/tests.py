e4d1f6f8b0d97ce8caf5db457511df4130b77192
jeffrey k eliasen <jeff@jke.net>
2018-10-01 19:43:52 -1000

    empty test file

---
+++ red-green-refactor/tests.py
@@ -0,0 +1,7 @@
+# password rules
+#
+# must be a string
+# must be at least 8 characters
+# must contain at least one lower-case letter
+# must contain at least one upper-case letter
+# must contain at least one number
