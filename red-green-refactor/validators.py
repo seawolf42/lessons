fd08ac8b128457b7781536231561a261eb0a645b
jeffrey k eliasen <jeff@jke.net>
2018-10-03 19:08:59 -1000

    refactor: simplify code

--- red-green-refactor/validators.py
+++ red-green-refactor/validators.py
@@ -1,12 +1,12 @@
+import re
+
+
+valid_regex = re.compile('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}')
+
+
 def password(value):
     if not isinstance(value, str):
         return False
-    if len(value) < 8:
-        return False
-    if value.upper() == value:
-        return False
-    if value.lower() == value:
-        return False
-    if len(set(value).intersection(set('1234567890'))) == 0:
-        return False
-    return True
+    if valid_regex.match(value):
+        return True
+    return False
