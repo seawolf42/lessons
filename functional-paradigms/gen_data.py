bf3dd865cffc52b497a6d61bc9cd0b013ad4f14d
jeffrey k eliasen <jeff@jke.net>
2020-10-07 13:42:25 -1000

    add additional file type for live demo

--- functional-paradigms/gen_data.py
+++ functional-paradigms/gen_data.py
@@ -1,5 +1,3 @@
-import io
-
 from tarfile import open as tar_open
 from zipfile import ZipFile
 
@@ -13,35 +11,52 @@ default_data = tuple((chr(i), i, hex(i), oct(i)) for i in range(ord('a'), ord('a
 #   ...
 # )
 
+
 def create(data=default_data):
-    for extension, data_to_rows in (('csv', stringified_to_csv_rows), ('tsv', stringified_to_tsv_rows)):
+    for extension, data_to_rows in (
+        ('csv', stringified_to_csv_rows),
+        ('psv', stringified_to_psv_rows),
+        ('tsv', stringified_to_tsv_rows),
+    ):
         filename = f'abc.{extension}'
         blob = content_to_blob(rows_to_content(data_to_rows(stringified(data))))
         for writer in (raw_writer, zip_writer, tgz_writer):
             writer(filename, blob)
 
+
 def stringified(data):
     return ((str(field) for field in row) for row in data)
+
 
 def stringified_to_csv_rows(data):
     return (','.join(row) for row in data)
 
+
+def stringified_to_psv_rows(data):
+    return ('|'.join(row) for row in data)
+
+
 def stringified_to_tsv_rows(data):
     return ('\t'.join(row) for row in data)
+
 
 def rows_to_content(rows):
     return '\n'.join(rows)
 
+
 def content_to_blob(content):
     return content.encode()
+
 
 def raw_writer(filename, blob):
     with open(filename, 'wb') as fout:
         fout.write(blob)
 
+
 def tgz_writer(filename, blob):
     with tar_open(f'{filename}.tgz', 'w:gz') as fout:
         fout.add(filename)
+
 
 def zip_writer(filename, blob):
     with ZipFile(f'{filename}.zip', 'w') as fout:
