f4fd10421545d1d04e8b4bdb56a58679dfd6663a
jeffrey k eliasen <jeff@jke.net>
2020-10-16 17:27:19 -1000

    STASH

--- functional-paradigms/firstclass.py
+++ functional-paradigms/firstclass.py
@@ -1,40 +1,37 @@
 import functools
 
+NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
 # "novice" way to do build a list of even numbers
 
-
-def evens(start, stop):
-    # evens(1, 11) => [2, 4, 6, 8, 10]
+def evens(numbers):
     result = []
-    for i in range(start, stop):
-        if i % 2 == 0:
-            result.append(i)
+    for number in numbers:
+        if number % 2 == 0:
+            result.append(number)
     return result
 
-# comprehensions-based
+
+# evens(NUMBERS) => [2, 4, 6, 8, 10]
 
 
-def evens_list_comprehension(start, stop):
-    return [i * 2 for i in range(round(start / 2), stop // 2 + 1)]
-    # or:  [i for i in range(start, stop) if i % 2 == 0]
-
-# convert an iterable to a list
+def evens_list_comprehension(numbers):
+    return [number for number in numbers if number % 2 == 0]
 
 
-def evens_range_increment(start, stop):
-    return list(range(start + start % 2, stop, 2))
-
-# return a generator instead of a list
+def evens_filter(numbers):
+    return filter(lambda x: x % 2 == 0, numbers)
 
 
-def evens_generator(start, stop):
-    return range(start + start % 2, stop, 2)
-    # or:  filter(lambda x: x % 2 == 0, range(start, stop))
-
-# radical approach
+def evens_generator(numbers):
+    return (number for number in numbers if number % 2 == 0)
 
 
-def evens_generator_repeatable(start, stop):
-    # gen_1_to_11 = evens_generator_repeatable(1, 11) <- returns a callable
-    # list(gen_1_to_11()) => [2, 4, 6, 8 10] <- calling the callable
-    return functools.partial(range, start + start % 2, stop, 2)
+def evens_generator_repeatable(numbers):
+    return functools.partial(evens_generator, numbers)
+
+
+# evens_again = evens_generator_repeatable(NUMBERS) <- returns a callable
+# list(evens_again()) => [2, 4, 6, 8 10] <- calling the callable
+# list(evens_again()) => [2, 4, 6, 8 10] <- calling it again for a new iterable through the same list
